68d24c53adcfa71fbb8be0f72a91cfc26d22ff7e
Yansh Bhardwaj <yansh.b@momentus.in>
2025-11-12 21:49:20 +0530

    added header for the application

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from "./components/Layouts/Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <h2>Let's get started!</h2>
+    </div>
+  );
+}
+
+export default App;

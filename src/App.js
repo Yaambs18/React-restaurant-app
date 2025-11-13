1ce40e3196d2a557c4a22dcbf1a3e21f1b796f94
Yansh Bhardwaj <yansh.b@momentus.in>
2025-11-13 21:18:20 +0530

    added summary card

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/Layouts/Header";
+import SummaryCard from "./components/Meals/SummaryCard";
 
 function App() {
   return (
     <div>
       <Header />
+      <SummaryCard />
       <h2>Let's get started!</h2>
     </div>
   );
